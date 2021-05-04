c8eddbae381327cdf6c8f5c04265e210eb301ec3
siemens-c2c <joy_mon78@yahoo.co.in>
2021-05-04 17:50:21 +0530

    day-2 update-3

--- codes/day-2/js-concepts-app/source/filterArray.js
+++ codes/day-2/js-concepts-app/source/filterArray.js
@@ -1,12 +1,85 @@
-function filterData(input) {
+//OddLogic
+// var isOdd = function (num) {
+//     return num % 2 != 0 ? true : false;
+// }
+//arrow operator
+var isOdd = (num) => num % 2 != 0;
+
+//EvenLogic
+// var isEven = function (num) {
+//     return num % 2 == 0;
+// }
+
+// var isEven = (num) => {
+//     if (num % 2 == 0)
+//         return true;
+//     else
+//         return false;
+// }
+var isEven = num => num % 2 == 0;
+
+//Greater logic
+var isGreatherThanFive = function (num) {
+    return num > 5;
+}
+
+
+function filterData(inputArray, predicate) {
     var result = [];
-    for (var i = 0; i < input.length; i++) {
-        if (input[i] % 2 != 0) {
-            result.push(input[i])
+    for (var i = 0; i < inputArray.length; i++) {
+        var element = inputArray[i];
+        var status = predicate(element);
+        if (status) {
+            result.push(inputArray[i])
         }
     }
     return result;
 }
+
+//source of data
 var numbers = [1, 3, 2, 5, 4, 6, 7, 8, 0, 9]
-var filtered = filterData(numbers);
+//pass source of data and logic
+//var filtered = filterData(numbers, isGreatherThanFive);
+// var filtered = filterData(numbers, function (num) {
+//     return num > 3;
+// });
+var filtered = filterData(numbers, (num) => num % 2 != 0);
+
 console.log(filtered)
+
+var people = [{
+    name: 'joydip',
+    id: 1,
+    salary: 2000
+}, {
+    name: 'anil',
+    id: 2,
+    salary: 1000
+}, {
+    name: 'sunil',
+    id: 3,
+    salary: 3000
+}]
+
+var filteredBySalary = filterData(people, function (person) {
+    return person.salary >= 2000;
+})
+
+var filteredById = filterData(people, function (person) {
+    return person.id >= 2;
+})
+
+console.log(filteredBySalary)
+console.log(filteredById)
+
+var final = numbers.filter(function (num) {
+    return num % 2 == 0;
+})
+
+console.log(final)
+
+// numbers.sort(function (x, y) {
+//     return x - y;
+// })
+
+
--- codes/day-2/js-concepts-app/source/sortArray.js
+++ codes/day-2/js-concepts-app/source/sortArray.js
@@ -1,9 +1,10 @@
-/*
-function sortNumberArrayInAscendingOrder(array) {
+
+function sortInAscendingOrder(array, fn) {
     for (var i = 0; i < array.length; i++) {
         for (var j = i + 1; j < array.length; j++) {
-            var temp = 0;
-            if (array[i] > array[j]) {
+            var temp;
+            var status = fn(array[i], array[j]);
+            if (status > 0) {
                 temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
@@ -14,11 +15,33 @@ function sortNumberArrayInAscendingOrder(array) {
 
 var numbers = [1, 3, 4, 2, 6, 8, 5, 7, 0, 9];
 var ascendingInputArray = [...numbers];
-sortNumberArrayInAscendingOrder(ascendingInputArray);
+sortInAscendingOrder(ascendingInputArray, function (a, b) {
+    return a - b;
+});
 console.log(ascendingInputArray)
-console.log(numbers)
-*/
+//console.log(numbers)
 
+var people = [{
+    name: 'joydip',
+    id: 1,
+    salary: 2000
+}, {
+    name: 'anil',
+    id: 2,
+    salary: 1000
+}, {
+    name: 'sunil',
+    id: 2,
+    salary: 3000
+}]
+var copyOfPeople = [{ ...people[0] }, { ...people[1] }, { ...people[2] }]
+sortInAscendingOrder(copyOfPeople, function (person1, person2) {
+    return person1.salary - person2.salary;
+})
+
+console.log(copyOfPeople)
+
+/*
 function sortPeopleBySalary(array) {
     for (var i = 0; i < array.length; i++) {
         for (var j = i + 1; j < array.length; j++) {
@@ -61,3 +84,4 @@ console.log(copyOfPeople)
 // var copyOfJoydip = { ...people[0] }
 // var copyOfAnil = { ...people[1] }
 // var copyOfPeople = [copyOfJoydip, copyOfAnil];
+*/
