757318a1e4c4c9b3b61d6a924323c4eda849e6f7
joydip-git <34733254+joydip-git@users.noreply.github.com>
2021-05-12 18:30:16 +0530

    day-8 final update

--- codes/day-8/product-app/source/index.js
+++ codes/day-8/product-app/source/index.js
@@ -1,45 +1,8 @@
-import { add } from "./calculation";
-import { trainer, trainee, person } from "./person";
-
-//console.log(add(10, 20))
+import { trainer } from "./trainer";
+import { trainee } from "./trainee";
 
 const joydipTrainer = new trainer('joydip', 'Bangalore', 'JavaScript')
 console.log(joydipTrainer.show())
 
 const anilTrainee = new trainee('anil', 'Chennai', 'expleo groups', 'mobile')
 console.log(anilTrainee.show())
-
-/*
-Object.prototype.sayHi = function () {
-    console.log('Hi....')
-}
-console.log(Object.prototype)
-
-console.log(joydipTrainer.hasOwnProperty('sayHi'))
-console.log(trainer.prototype.hasOwnProperty('sayHi'))
-console.log(Object.prototype.hasOwnProperty('sayHi'))
-
-joydipTrainer.sayHi();
-
-var data = {
-    name: 'anil'
-}
-
-*/
-
-/*
-console.log('add prototype')
-console.log(add.prototype)
-
-console.log('trainer prototype')
-console.log(trainer.prototype)
-
-console.log('person prototype')
-console.log(person.prototype)
-
-console.log('object prototype')
-console.log(Object.prototype)
-*/
-/**
- * function Object(){}
- */
--- codes/day-8/product-app/source/person.js
+++ codes/day-8/product-app/source/person.js
@@ -1,37 +1,18 @@
+/*
 export function person(name, location) {
     this.name = name;
     this.location = location;
-    // this.show = function () {
-    //     return `${this.name}, ${this.location}`
-    // }
 }
 person.prototype.show = function () {
     return `${this.name}, ${this.location}`
 }
-
-export function trainer(name, location, subjectToTeach) {
-    person.call(this, name, location);
-    // let boundPersonFn = person.bind(this)
-    // boundPersonFn(name, location)
-    this.subjectToTeach = subjectToTeach;
-    this.show = function () {
-        // let partialInfo = person.prototype.show.apply(this)
-        let partialInfo = this.__proto__.show.apply(this)
-        return `${partialInfo}, ${this.subjectToTeach}`
+*/
+export class person {
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+    }
+    show() {
+        return `${this.name}, ${this.location}`
     }
 }
-
-Object.setPrototypeOf(trainer.prototype, person.prototype)
-
-export function trainee(name, location, company, domain) {
-    person.call(this, name, location)
-    this.company = company;
-    this.domain = domain;
-    this.show = function () {
-        //let partialInfo = person.prototype.show.apply(this)
-        let partialInfo = this.__proto__.show.apply(this)
-        return `${partialInfo}, ${this.company}, ${this.domain}`
-    }
-}
-
-Object.setPrototypeOf(trainee.prototype, person.prototype)
