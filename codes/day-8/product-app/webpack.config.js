e208f6a298f05d6d60e9b8a446719f4627887d49
joydip-git <34733254+joydip-git@users.noreply.github.com>
2021-05-12 17:27:28 +0530

    prototype update

--- codes/day-8/product-app/webpack.config.js
+++ codes/day-8/product-app/webpack.config.js
@@ -1,14 +1,17 @@
 const pathModule = require('path')
 const htmlPluginModule = require('html-webpack-plugin')
 const cleanPluginModule = require('clean-webpack-plugin')
-
+/**
+ * entry:{
+ * main: pathModule.resolve(__dirname, 'source', 'index.js'),
+   calc: pathModule.resolve(__dirname, 'source', 'calculation.js')
+ }
+ */
 const webpackConfigObj = {
-    entry: {
-        main: pathModule.resolve(__dirname, 'source', 'index.js'),
-        calc: pathModule.resolve(__dirname, 'source', 'calculation.js')
-    },
+    entry: pathModule.resolve(__dirname, 'source', 'index.js'),
     output: {
-        filename: '[name].bundle.js',
+        // filename: '[name].bundle.js',
+        filename: 'product.bundle.js',
         path: pathModule.resolve(__dirname, 'dist')
     },
     module: {
