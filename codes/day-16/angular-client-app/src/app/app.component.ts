b1d2bb76db28b7f2f6615c300cb3038bf4f83b40
joydip-git <34733254+joydip-git@users.noreply.github.com>
2021-05-25 12:07:46 +0530

    day-16 update-2

--- codes/day-16/angular-client-app/src/app/app.component.ts
+++ codes/day-16/angular-client-app/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from "@angular/core";
     selector: 'app-root'
 })
 export class AppComponent {
-
+    //type inference
+    title = 'Welcome to Contact Management System'
+    year = 2021
+    paraWidth = 200
+    imgPath = '../assets/images/headset.jfif'
+    imgWidth = 50
+    imgMargin = 2
+    // changeHeaderValue(element: HTMLInputElement): void {
+    //     console.log(element)
+    //     this.title = element.value
+    // }
+    changeHeaderValue(val: string): void {
+        this.title = val
+    }
 }
